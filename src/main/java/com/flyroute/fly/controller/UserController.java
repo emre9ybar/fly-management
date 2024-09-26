59d4ff7b21e4dd22519e9711fb2ef6344752f75c
Emre Aybar <aybaremre5@gmail.com>
2024-09-26 14:22:20 +0300

    user  controller update

--- src/main/java/com/flyroute/fly/controller/UserController.java
+++ src/main/java/com/flyroute/fly/controller/UserController.java
@@ -2,6 +2,7 @@ package com.flyroute.fly.controller;
 
 import com.flyroute.fly.dto.request.CreateUserRequest;
 import com.flyroute.fly.dto.request.UpdateUserRequest;
+import com.flyroute.fly.dto.request.UserDeleteRequest;
 import com.flyroute.fly.dto.response.GetUsersListResponse;
 import com.flyroute.fly.dto.response.UserGetByIdResponse;
 import com.flyroute.fly.service.UserService;
@@ -14,49 +15,40 @@ import java.util.List;
 @RestController
 @RequestMapping("/api/users")
 public class UserController {
-    private final   UserService userService;
+
+    private final UserService userService;
 
     public UserController(UserService userService) {
         this.userService = userService;
     }
-    //trylar cacth serviste yazılacak . log serviste .
-    //Exception sınıfı oluşturulacak orada .
+
     @PostMapping
-    public ResponseEntity<CreateUserRequest> add(@RequestBody CreateUserRequest userDto) {
-        try {
-            userService.add(userDto);
-            return ResponseEntity.status(HttpStatus.CREATED).build();
-        } catch (Exception e) {
-            return ResponseEntity.status(HttpStatus.BAD_REQUEST).build();
-        }
+    public ResponseEntity<Void> add(@RequestBody CreateUserRequest userDto) {
+        userService.add(userDto);
+        return ResponseEntity.status(HttpStatus.CREATED).build();
     }
 
     @GetMapping
     public ResponseEntity<List<GetUsersListResponse>> getAllUserList() {
-        try {
-            List<GetUsersListResponse> userDtos = userService.getAllUserList();
-            return ResponseEntity.ok(userDtos);
-        } catch (Exception e) {
-            return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).build();
-        }
-
+        List<GetUsersListResponse> userDtos = userService.getAllUserList();
+        return ResponseEntity.ok(userDtos);
     }
 
     @GetMapping("/{id}")
-    public ResponseEntity<UserGetByIdResponse> getUserById(@PathVariable long id){
-       UserGetByIdResponse UserGetByIdResponse = userService.getUserById(id);
-        return ResponseEntity.ok(UserGetByIdResponse) ;
+    public ResponseEntity<UserGetByIdResponse> getUserById(@PathVariable long id) {
+        UserGetByIdResponse userGetByIdResponse = userService.getUserById(id);
+        return ResponseEntity.ok(userGetByIdResponse);
     }
 
     @PutMapping("/{id}")
-    public void update(@RequestBody UpdateUserRequest UpdateUserRequest){
-        userService.updateUser(UpdateUserRequest);
+    public ResponseEntity<Void> update(@RequestBody UpdateUserRequest updateUserRequest) {
+        userService.updateUser(updateUserRequest);
+        return ResponseEntity.ok().build();
     }
 
     @DeleteMapping("/{id}")
-    public void delete(@PathVariable long id){
-        userService.deleteUser(id);
+    public ResponseEntity<Void> delete(@RequestBody UserDeleteRequest deleteRequest) {
+        userService.deleteUser(deleteRequest.getId());
+        return ResponseEntity.ok().build();
     }
-
-
 }
